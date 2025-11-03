e294f3c802b22b780b12d306a35e4e402e68e7fe
tirucloud <137248129+tirucloud@users.noreply.github.com>
2025-11-03 11:48:36 +0530

    Update jenkins.sh

--- iac/jenkins.sh
+++ iac/jenkins.sh
@@ -8,8 +8,6 @@ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt install jenkins
-sudo apt update
 sudo apt install jenkins -y
 sudo systemctl enable jenkins
 
